10708f9a9de01998a2586eda4e64442b73fb59de
Justin Heath <just.coer@outlook.com>
2021-08-04 12:53:30 +0100

    Day 1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Feature/Routings/about/about.component';
+import { GalleryComponent } from './Feature/Routings/gallery/gallery.component';
+import { HistoryComponent } from './Feature/Routings/history/history.component';
+import { HomeComponent } from './Feature/Routings/home/home.component';
+import { PageNotFoundComponent } from './Feature/_page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'home', component: HomeComponent },
+
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(appRoutes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavBarComponent } from './Feature/_nav-bar/nav-bar.component';
+import { UsersComponent } from './Feature/_users/users.component';
+import { AboutComponent } from './Feature/Routings/about/about.component';
+import { GalleryComponent } from './Feature/Routings/gallery/gallery.component';
+import { HistoryComponent } from './Feature/Routings/history/history.component';
+import { HomeComponent } from './Feature/Routings/home/home.component';
+import { PageNotFoundComponent } from './Feature/_page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    GalleryComponent,
+    HistoryComponent,
+    AboutComponent,
+    UsersComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
