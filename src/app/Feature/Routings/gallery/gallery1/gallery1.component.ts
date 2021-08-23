f37575c851ac7d1e0bb3626ce968321155ca49d6
Justin Heath <just.coder@outlook.com>
2021-08-23 20:42:05 +0100

    ask advice off hugh

--- src/app/Feature/Routings/gallery/gallery1/gallery1.component.ts
+++ src/app/Feature/Routings/gallery/gallery1/gallery1.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GalleryServiceService } from '../_Services/gallery-service.service';
 
-import GallaryPicture from '../../../../../assets/DB_Gallery_Pictures.json';
+// import GallaryPicture from '../../../../../assets/DB_Gallery_Pictures.json';
 
 import { I_Picture } from '../_Model/galleryModel';
 import { I_Video } from '../_Model/galleryModel';
@@ -13,9 +13,8 @@ import { I_Video } from '../_Model/galleryModel';
 })
 export class Gallery1Component implements OnInit {
   title: string = `angular-responsive-carousel`;
-  dataSet = GallaryPicture;
-  dataSetNew!: I_Picture[] | I_Video[];
-  dataSet2: any = [];
+  // dataSet = GallaryPicture;
+  dataSet: any = [];
   valueH = 550;
   wd = 1000;
   // cells = 5;
@@ -28,23 +27,19 @@ export class Gallery1Component implements OnInit {
   ap = true;
 
   constructor(private galleryServiceService: GalleryServiceService) {
-    // this.galleryServiceService.dataMessage$.subscribe(
-    //   (temp: I_Picture[] | I_Video[]) => {
-    //     this.dataSetNew = temp;
-    //     // console.log(this.dataSetNew);
-    //     // console.log(dataTest);
-    //     temp.forEach((d: I_Picture | I_Video) => {
-    //       console.log(d);
-    //       this.dataSet2.push({ path: d.url });
-    //       console.log(this.dataSet2);
-    //     });
-    //   }
-    // );
+    this.galleryServiceService.dataMessage$.subscribe(
+      (serviceData: I_Picture[] | I_Video[]) => {
+        // console.log(serviceData);
+        this.createURLList(serviceData);
+      }
+    );
   }
 
-  ngOnInit(): void {
-    this.dataSet.forEach((d: I_Picture | I_Video) => {
-      this.dataSet2.push({ path: d.url });
+  ngOnInit(): void {}
+
+  createURLList(d: I_Picture[] | I_Video[]) {
+    d.forEach((d: I_Picture | I_Video) => {
+      this.dataSet.push({ path: d.url });
     });
   }
 }
--- src/app/Feature/Routings/gallery/gallery2/gallery2.component.ts
+++ src/app/Feature/Routings/gallery/gallery2/gallery2.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { GalleryServiceService } from '../_Services/gallery-service.service';
 
 import GallaryData from '../../../../../assets/DB_Gallery_Pictures.json';
+
+import { I_Picture } from '../_Model/galleryModel';
+import { I_Video } from '../_Model/galleryModel';
 
 @Component({
   selector: 'app-gallery2',
@@ -9,10 +13,15 @@ import GallaryData from '../../../../../assets/DB_Gallery_Pictures.json';
 })
 export class Gallery2Component implements OnInit {
   title: string = `Bootstrap Carousel`;
-  dataSet = GallaryData;
+  dataSet: any = [];
 
-  constructor() {
-    // console.log(GallaryData);
+  constructor(private galleryServiceService: GalleryServiceService) {
+    this.galleryServiceService.dataMessage$.subscribe(
+      (serviceData: I_Picture[] | I_Video[]) => {
+        // console.log(serviceData);
+        this.dataSet = serviceData;
+      }
+    );
   }
 
   ngOnInit(): void {}
